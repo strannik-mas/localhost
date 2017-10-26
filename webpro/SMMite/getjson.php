f334f5137454fd9c31942f42aef1b767cb1d94ff
Alexander <mas_m@ukr.net>
2017-10-27 00:50:15 +0300

    начало разработки тестовой задачи по форме обратной связи

--- webpro/SMMite/getjson.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $params = array(
-        'vkGroupId'     => $_GET['vkGroupId']
-    );
-    $jsonData = file_get_contents($_GET['url'] . '?' . urldecode(http_build_query($params)));
-    echo $jsonData;
-?>

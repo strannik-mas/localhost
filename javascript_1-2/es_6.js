44b94ff805390476c888f0fd89575860f4d77782
Alexander <mas_m@ukr.net>
2017-12-12 23:45:05 +0200

    finish es_6

--- javascript_1-2/es_6.js
+++ javascript_1-2/es_6.js
@@ -67,3 +67,104 @@ let obj = {
 };
 obj.each();
 */
+//параметры по умолчанию
+/* function foo(x, y){
+	x = x || 1;
+	y = y || 0;
+} 
+function foo(x, y=2, z=3){
+	print(x+y+z);
+}
+foo(1);
+function foo(x, y, ...a){
+	// var a = Array.prototype.slice.call(arguments, 2);
+	print(a);
+}
+foo(1,2,3,4,5,6)
+var p = ["a", "b", "c"];
+var x = [1,2,...p];
+//тоже самое 
+// var x = x.concat(p);
+var s = "hello";
+var a = [...s];
+print(a);
+var name = "John", age = 25;
+var s = `hello ${name}. ${age +1}`;
+// String.raw `Hello\nworld`;
+console.log(String.raw `Hello\nworld`);
+var x=1, y=2;
+var obj = {
+	// x:x,
+	// y:y
+	x,y
+}
+print(obj.x);
+var [a,,c] = [1,2,3];		//аналог list в php
+[a,c] = [c,a];
+var {x,y,z} = {x:1, y:2, z:3};
+// print(a,c);
+print(x,y,z);*/
+/* function foo({a:x, b:y}){}
+foo({a:x, b:y}); */
+/*
+let x = Symbol("this is x");		//для уникальности
+let y = Symbol("this is y");
+print(x==y);
+print(x.toString());
+var o = {};
+o[x] = 3;
+*/
+/*
+//новые структуры
+let m = new Map();
+m.set('name', 'John');
+m.set('age', 25);
+// print(m.size);
+// print(m.get('name'));
+let s = new Set("hello");
+s.add(1).add(2).add('s').add(2);
+print(Array.from(s));
+// print(s.size);
+*/
+/*
+class Shape{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+	move(x, y){
+		this.x += x;
+		this.y += y;
+	}
+}
+let s = new Shape(10, 20);
+s.move(3,49);
+// console.log(s.x);
+class Rect extends Shape{
+	constructor(x, y, w, h){
+		super(x,y);
+		this.w = w;
+		this.h = h;
+	}
+	static foo(){
+		return 100;
+	}
+}
+Rect.foo();
+// let r = new Rect(10,29, 100,200);
+*/
+class User{
+	constructor(){
+		this._name = "";
+	}
+	set name (n){
+		this._name = n.toUpperCase();
+	}
+	get name (){
+		return this._name;
+	}
+}
+let u = new User();
+u.name = "John";
+console.log(u.name);
+console.log(Number.isNaN(1*'d'));
